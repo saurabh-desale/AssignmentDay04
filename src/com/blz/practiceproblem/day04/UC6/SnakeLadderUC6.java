4eeea516bf9894787be46262e88b7bb0fd104758
Saurabh <saurabhdesale007@gmail.com>
2021-08-01 11:45:32 +0530

    Add: number of times dice rotated

---
+++ src/com/blz/practiceproblem/day04/UC6/SnakeLadderUC6.java
@@ -0,0 +1,44 @@
+package com.blz.practiceproblem.day04.UC6;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class SnakeLadderUC6 {
+    public static void main(String[] args) {
+        int firstPlayer =0;
+       firstPlayerPosition(firstPlayer);
+	}
+
+	public static void firstPlayerPosition(int firstPlayer) {
+		int n = 0;
+		do {
+			n++;
+		int randomNum = ThreadLocalRandom.current().nextInt(1,6+1);
+       System.out.println(+randomNum);
+       int choice = ThreadLocalRandom.current().nextInt(1,3+1);
+       switch(choice) {
+       case 1:System.out.println("noPlay");
+       firstPlayer=0+firstPlayer;       
+    	   break;
+       case 2:System.out.println("Ladder");
+       firstPlayer=randomNum+firstPlayer;
+       if(firstPlayer>100) {
+              firstPlayer=firstPlayer-randomNum;    	   
+       }
+       break;
+       case 3:System.out.println("Snake");
+       firstPlayer=firstPlayer-randomNum;
+       if(firstPlayer<=0) {
+    	   firstPlayer=0;
+       }
+       break;
+       }
+    	   
+       
+       System.out.println("position of first player position is: "+firstPlayer);
+		}while (firstPlayer<100);
+		System.out.println("dice rolled :"+n+"times");
+	}
+
+
+
+}
