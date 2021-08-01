2d501981a13cd79e030ed5f58bcb9703d09bab08
Saurabh <saurabhdesale007@gmail.com>
2021-08-01 11:30:46 +0530

    Added first player

---
+++ src/com/blz/practiceproblem/day04/UC1/SnakeladderUC1.java
@@ -0,0 +1,10 @@
+package com.blz.practiceproblem.day04.UC1;
+
+public class SnakeladderUC1 {
+	int firstPlayer=0;
+    int winNumber=100;
+public static void main(String[] args) {
+System.out.println("this is player 1");
+}
+
+}
